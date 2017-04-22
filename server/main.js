026fefdda839b7b9bce8735a97f3a3eb71093394
Abhishek Sharma <hello.abhishek@outlook.com>
2017-04-22 12:26:36 +0530

    insecure package removed

--- server/main.js
+++ server/main.js
@@ -1,3 +1,23 @@
 var list = new Mongo.Collection('list');
 
 
+Meteor.methods({
+  addToDo: function (name) {
+    if (!Meteor.userId()) { 
+      throw new Meteor.Error('Not Authorized');
+    }
+    let temp = new Date();
+    list.insert({
+      taskName: name,
+      date: temp,
+      userId: Meteor.userId(),
+      userName: Meteor.user().username
+    });
+  },
+  deleteToDo: function (taskid) {
+    list.remove(taskid);
+  },
+  updateStatus: function (taskid, checkedvalue) {
+    list.update(taskid, { $set: { checked: checkedvalue } });
+  }
+});
